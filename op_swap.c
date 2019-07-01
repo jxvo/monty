2e49dc07ea4f538763d555439a0b18db3109b1e6
Jonathan Vasquez <727@holberton.com>
2019-07-01 07:49:00 +0000

    Fixed error handling... hopefully

--- op_swap.c
+++ op_swap.c
@@ -11,7 +11,7 @@ void op_swap(stack_t **head, unsigned int line_number)
 {
 	int temp_n;
 
-	if (!(*head)->next || !head || !*head)
+	if (!head || !*head || (*head)->next == NULL)
 		swap_error(head, line_number);
 	temp_n = (*head)->n;
 	(*head)->n = (*head)->next->n;
